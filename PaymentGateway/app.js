b5267a2a9c73e71ee770c0c7656e6340802b8814
Suelen Rodrigues <rodrigues.su@outlook.com>
2021-08-19 19:21:01 +0100

    Changes made to host on Heroku

---
+++ PaymentGateway/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+
+const app = express();
+
+app.set("view engine", "ejs"); 
+app.use(express.static("public"));
+
+app.get('/', function(req,res){
+	res.render('index');
+});
+
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, ()=> console.log("Running...", port));
